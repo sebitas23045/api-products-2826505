47868aeaaca356cebd7719a8b3f0209f0ce1fb34
= <=>
2024-07-24 17:47:29 -0500

    servicio creado

--- src/main/java/com/api/java/api_products_2826505/services/ProductService.java
+++ src/main/java/com/api/java/api_products_2826505/services/ProductService.java
@@ -1,5 +1,27 @@
 package com.api.java.api_products_2826505.services;
 
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.api.java.api_products_2826505.entities.Product;
+import com.api.java.api_products_2826505.repositories.ProductRepository;
+
+@Service
 public class ProductService {
+
+    //dependencia importanto de Autowired
+    @Autowired
+    private ProductRepository repository;
+
+    //meotods 
+    //Read
+    List<Product> FindAll() {
+
+        return (List<Product>) repository.findAll();
+
+    }
 
 }
